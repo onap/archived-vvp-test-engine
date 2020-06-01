14df46b7b0166c05af9056ba71f1b6bac05e4f2d
stark, steven <steven.stark@att.com>
2020-06-01 09:38:11 -0700

    [VVP] bug fix for empty project code
    Issue-ID: VVP-419
    Change-Id: Ic65674f2ee924f43b7ba6d8cc63ee686723f8fa5
    Signed-off-by: stark, steven <steven.stark@att.com>

--- onap-client/setup.py
+++ onap-client/setup.py
@@ -47,7 +47,7 @@ for file in os.listdir("etc/payloads"):
 
 setuptools.setup(
     name="onap-client",
-    version="0.6.2",
+    version="0.6.3",
     author="Steven Stark",
     author_email="steven.stark@att.com",
     description="Python API wrapper for ONAP applications",
